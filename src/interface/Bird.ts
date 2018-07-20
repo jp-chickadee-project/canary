a210c0f52e0edbb46239d6faa0793b2ec9f3eb69
David Germain <davidispizza@gmail.com>
2018-07-20 16:30:30 -0400

    Create bird interface

---
+++ src/interface/Bird.ts
@@ -0,0 +1,47 @@
+
+export interface Measurements {
+  bib: number;
+  bill: {
+    depth: number;
+    width: number;
+    length: number;
+  };
+  tarsus: number;
+  cap: number;
+  longestSecondary: number;
+  wingChord: number;
+  tail: number;
+  weight: number;
+}
+
+export interface Details {
+  banders: string;
+  weather: string;
+  notes: string;
+  tissueSample: string;
+  captureSite: string;
+  bandNumber: string;
+  timestamps: {
+    log: number;
+    capture: number;
+    enter: number;
+    exit: number;
+  };
+  legRightTop: string;
+  legRightBottom: string;
+  legLeftTop: string;
+  legLeftBottom: string;
+}
+
+export type species = 'RBNU' | 'something-else';
+export type sex = 'male' | 'female' | 'unknown';
+
+export interface Bird {
+  id: string;
+  bandCombo: string;
+  release: number;
+  species: species;
+  sex: sex;
+  measurements?: Measurements;
+  details?: Details;
+}
