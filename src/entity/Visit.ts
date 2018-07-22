c9e305f5fb8332f9406d191c22426215a2e72df0
David Germain <davidispizza@gmail.com>
2018-07-21 23:18:55 -0400

    Rename columns

--- src/entity/Visit.ts
+++ src/entity/Visit.ts
@@ -7,11 +7,12 @@ export class Visit {
   @PrimaryGeneratedColumn() private id: number;
   // TODO: make migration for auto id.
   // TODO: how is this table sorted?
+  // TODO: not sure if bird and feeder should be foreign keys because then we would lose ghost reads 
 
-  @Column({ type: 'varchar', length: 16, }) rfid: string;
-  @Column({ type: 'char', length: 4, }) feederID: string;
+  @Column({ type: 'varchar', length: 16, }) bird: string;
+  @Column({ type: 'char', length: 4, }) feeder: string;
 
-  @Column({ type: 'bigint', precision: 20, nullable: false, }) visitTimestamp: number;
+  @Column({ type: 'bigint', precision: 20, nullable: false, }) timestamp: number;
 
   @Column({ type: 'varchar', length: 32, }) bandCombo: string;
 }
