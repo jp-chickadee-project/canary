fd9b4692c0658c9c599151cc116d32258d4d6e60
David Germain <davidispizza@gmail.com>
2018-07-21 23:43:30 -0400

    Fix lat and long

--- src/migration/1532229629981-convert-feeder-coordinates-to-double.ts
+++ src/migration/1532229629981-convert-feeder-coordinates-to-double.ts
@@ -3,32 +3,8 @@ import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 export class convertFeederCoordinatesToDouble1532229629981 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-      await queryRunner.changeColumn('feeders',
-        new TableColumn({
-          name: 'longitude',
-          type: 'decimal',
-          precision: 10,
-          scale: 7,
-        }),
-        new TableColumn({
-          name: 'longitude',
-          type: 'double',
-          isNullable: false,
-        }),
-      );
-      await queryRunner.changeColumn('feeders',
-      new TableColumn({
-        name: 'latitude',
-        type: 'decimal',
-        precision: 10,
-        scale: 7,
-      }),
-      new TableColumn({
-        name: 'latitude',
-        type: 'double',
-        isNullable: false,
-      }),
-    );
+      await queryRunner.query('ALTER TABLE feeders MODIFY COLUMN latitude DOUBLE NOT NULL');
+      await queryRunner.query('ALTER TABLE feeders MODIFY COLUMN longitude DOUBLE NOT NULL');
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
