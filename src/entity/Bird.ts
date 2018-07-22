3a28a7461ca9061531381a7ac151d2b0052fa896
David Germain <davidispizza@gmail.com>
2018-07-21 22:19:09 -0400

    Make most properties nullable

--- src/entity/Bird.ts
+++ src/entity/Bird.ts
@@ -8,43 +8,43 @@ export class Bird {
 
   @Column({ type: 'varchar', length: 8, nullable: true, }) species: string;
 
-  @Column({ type: 'varchar', length: 64, }) captureSite: string;
-  @Column({ type: 'varchar', length: 64, }) bandNumber: string;
+  @Column({ type: 'varchar', length: 64, nullable: true, }) captureSite: string;
+  @Column({ type: 'varchar', length: 64, nullable: true, }) bandNumber: string;
 
-  @Column({ type: 'varchar', length: 8, }) legRightTop: string;
-  @Column({ type: 'varchar', length: 8, }) legRightBottom: string;
-  @Column({ type: 'varchar', length: 8, }) legLeftTop: string;
-  @Column({ type: 'varchar', length: 8, }) legLeftBottom: string;
+  @Column({ type: 'varchar', length: 8, nullable: true, }) legRightTop: string;
+  @Column({ type: 'varchar', length: 8, nullable: true, }) legRightBottom: string;
+  @Column({ type: 'varchar', length: 8, nullable: true, }) legLeftTop: string;
+  @Column({ type: 'varchar', length: 8, nullable: true, }) legLeftBottom: string;
 
-  @Column({ type: 'int', precision: 10, }) tailLength: number;
-  @Column({ type: 'int', precision: 10, }) wingChordLength: number;
-  @Column({ type: 'int', precision: 10, }) longestSecondary: number;
+  @Column({ type: 'int', precision: 10, nullable: true, }) tailLength: number;
+  @Column({ type: 'int', precision: 10, nullable: true, }) wingChordLength: number;
+  @Column({ type: 'int', precision: 10, nullable: true, }) longestSecondary: number;
 
-  @Column({ type: 'decimal', precision: 5, scale: 2, }) billDepth: number;
-  @Column({ type: 'decimal', precision: 5, scale: 2, }) billWidth: number;
-  @Column({ type: 'decimal', precision: 5, scale: 2, }) billLength: number;
-  @Column({ type: 'decimal', precision: 5, scale: 2, }) bibWidth: number;
-  @Column({ type: 'decimal', precision: 5, scale: 2, }) capLength: number;
-  @Column({ type: 'decimal', precision: 5, scale: 2, }) tarsusLength: number;
+  @Column({ type: 'decimal', precision: 5, scale: 2, nullable: true, }) billDepth: number;
+  @Column({ type: 'decimal', precision: 5, scale: 2, nullable: true, }) billWidth: number;
+  @Column({ type: 'decimal', precision: 5, scale: 2, nullable: true, }) billLength: number;
+  @Column({ type: 'decimal', precision: 5, scale: 2, nullable: true, }) bibWidth: number;
+  @Column({ type: 'decimal', precision: 5, scale: 2, nullable: true, }) capLength: number;
+  @Column({ type: 'decimal', precision: 5, scale: 2, nullable: true, }) tarsusLength: number;
 
-  @Column({ type: 'decimal', precision: 10, scale: 4, }) bagWeight: number;
-  @Column({ type: 'decimal', precision: 10, scale: 4, }) bagAndBirdWeight: number;
-  @Column({ type: 'decimal', precision: 10, scale: 4, }) birdWeight: number;
+  @Column({ type: 'decimal', precision: 10, scale: 4, nullable: true, }) bagWeight: number;
+  @Column({ type: 'decimal', precision: 10, scale: 4, nullable: true, }) bagAndBirdWeight: number;
+  @Column({ type: 'decimal', precision: 10, scale: 4, nullable: true, }) birdWeight: number;
 
-  @Column({ type: 'varchar', length: 32, }) tissueSample: string;
+  @Column({ type: 'varchar', length: 32, nullable: true, }) tissueSample: string;
 
-  @Column({ type: 'varchar', length: 16, }) suspectedSex: string;
+  @Column({ type: 'varchar', length: 16, nullable: true, }) suspectedSex: string;
 
-  @Column({ type: 'bigint', precision: 20, }) logTimestamp: number;
+  @Column({ type: 'bigint', precision: 20, nullable: true, }) logTimestamp: number;
   @Column({ type: 'bigint', precision: 20, nullable: true, }) captureTimestamp: number;
   @Column({ type: 'bigint', precision: 20, nullable: true, }) netEnterTimestamp: number;
   @Column({ type: 'bigint', precision: 20, nullable: true, }) netExitTimestamp: number;
   @Column({ type: 'bigint', precision: 20, nullable: true, }) releasedTimestamp: number;
 
-  @Column({ type: 'varchar', length: 256, }) notes: string;
-  @Column({ type: 'varchar', length: 256, }) weather: string;
+  @Column({ type: 'varchar', length: 256, nullable: true, }) notes: string;
+  @Column({ type: 'varchar', length: 256, nullable: true, }) weather: string;
 
-  @Column({ type: 'varchar', length: 64, }) banders: string;
+  @Column({ type: 'varchar', length: 64, nullable: true, }) banders: string;
 
-  @Column({ type: 'varchar', length: 32, }) bandCombo: string; 
+  @Column({ type: 'varchar', length: 32, nullable: true, }) bandCombo: string; 
 }
