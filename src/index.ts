9fd407c9809c3d72000d9db10a3785e6e842f186
David Germain <davidispizza@gmail.com>
2018-07-21 20:38:06 -0400

    Server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import 'reflect-metadata';
 
-import { createConnection, Repository, Connection } from 'typeorm';
+import { createConnection } from 'typeorm';
+
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+const app: express.Application = express();
+
+app.use(bodyParser.json());
 
 import {
   Bird,
@@ -8,17 +15,41 @@ import {
   Visit,
 } from './entity';
 
-function connect(stuff) {
-  stuff.entities = [
-    __dirname + '/entity/'
-  ];
-  return createConnection(stuff);
-}
+console.log('test');
 
-export {
-  Bird,
-  Feeder,
-  Visit,
-  Repository,
-  connect,
-};
+createConnection()
+  .then(async connection => {
+    const birds = connection.getRepository(Bird);
+    const feeders = connection.getRepository(Feeder);
+    const visits = connection.getRepository(Visit);
+
+    app.get('/api/health', (req, res) => {
+      res.write('hello');
+      res.end();
+    });
+
+    app.get('/api/birds', (req, res) => {
+      birds.find()
+        .then(birds => res.json(birds))
+        .catch(error => console.log(error));
+    });
+
+    app.get('/api/birds/:rfid', (req, res) => {
+      const rfid: string = req.params.rfid;
+      birds.findOne(rfid)
+        .then(bird => res.json(bird))
+        .catch(error => console.log(error));
+    });
+    
+    app.get('/api/visits', (req, res) => {
+      visits.find()
+        .then((visits) => res.json(visits))
+        .catch(error => console.log(error));
+    });
+    
+    const port = 8155;
+    app.listen(port, () => {
+      console.log(`running on http://localhost:${port}/`);
+    });
+  })
+  .catch(error => console.log(error));
